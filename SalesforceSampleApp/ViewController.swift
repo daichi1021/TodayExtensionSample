8cbd9ba277947884767c1e1418af19a88e2a0f11
溝口 大地 <dmizoguchi@terrasky.com>
2016-10-24 18:07:20 +0900

    Initial Commit

---
+++ SalesforceSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalesforceSampleApp
+//
+//  Created by 溝口大地 on 2016/10/24.
+//  Copyright © 2016年 Daichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
